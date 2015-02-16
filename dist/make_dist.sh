a7e296e9a6b85f17c1922b4fc97504ff07eb1309
Jens Kutilek <webmail@kutilek.de>
2015-02-16 15:09:08 +0100

    Update build scripts

--- dist/make_dist.sh
+++ dist/make_dist.sh
@@ -1,18 +1,45 @@
 #!/bin/sh
 
+# clean up old dist files
+
+rm "Fira System Fonts.pkg"
 rm "fira-system-fonts.zip"
+rm "fonts.pkg"
+
 sudo rm "Fira System Fonts/FiraSystem.ttc"
-ftxmakettc ../ttf/FiraSystem-Regular.ttf ../ttf/FiraSystem-Bold.ttf ../ttf/FiraSystem-Light.ttf ../ttf/FiraSystem-Medium.ttf "Fira System Fonts/FiraSystem.ttc"
+
+# build TrueType collection
+
+ftxmakettc \
+  ../ttf/FiraSystem-Regular.ttf \
+  ../ttf/FiraSystem-Bold.ttf \
+  ../ttf/FiraSystem-Light.ttf \
+  ../ttf/FiraSystem-Medium.ttf \
+  "Fira System Fonts/FiraSystem.ttc"
+
 sudo chown root:wheel "Fira System Fonts/FiraSystem.ttc"
 
 zip -x \*.DS\* -r "fira-system-fonts.zip" "Fira System Fonts"
 
-pkgbuild --root "Fira System Fonts" \
-  --version "3.113" \
+# build packages
+
+pkgbuild \
+  --root "Fira System Fonts" \
+  --version "3.112" \
   --filter ".txt" \
   --filter "._" \
-  --identifier "de.kutilek.fonts.firasystem" \
+  --identifier "de.kutilek.fonts.firasystem.pkg" \
   --install-location "/Library/Fonts" \
+  "fonts.pkg"
+
+productbuild \
+  --distribution distribution.xml \
+    --resources resources \
   "Fira System Fonts.pkg"
 
 zip "fira-system-fonts-installer.zip" "Fira System Fonts.pkg"
+
+# clean up intermediate packages
+
+rm "Fira System Fonts.pkg"
+rm "fonts.pkg"
