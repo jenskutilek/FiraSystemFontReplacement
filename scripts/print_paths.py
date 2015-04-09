72055bf45f3fa3656ec008e9b1fa08bc357e0445
Jens Kutilek <webmail@kutilek.de>
2015-04-10 00:52:42 +0200

    Add helper script

---
+++ scripts/print_paths.py
@@ -0,0 +1,19 @@
+#MenuTitle: Print Paths
+
+glyphs = [".notdef", "apple"]
+
+def getPaths(glyph_name):
+	layer = Glyphs.font.glyphs[glyph_name].layers[0]
+
+	result = "%s = [" % glyph_name
+
+	for path in layer.paths:
+		result += "["
+		for node in path.nodes:
+			result += "((%i, %i), %s), " % (node.x, node.y, node.type)
+		result += "], "
+	result += "]"
+	return result
+
+for glyph_name in glyphs:
+	print getPaths(glyph_name)
