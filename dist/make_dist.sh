271122013103f8f5baca159bef7fc9f596390914
Jens Kutilek <webmail@kutilek.de>
2017-07-18 22:23:03 +0200

    First working version for Sierra

--- dist/make_dist.sh
+++ dist/make_dist.sh
@@ -5,47 +5,13 @@
 rm "Fira System Fonts.pkg"
 rm "fonts.pkg"
 
-sudo rm Fira\ System\ Fonts/*.otf
+sudo rm Fira\ System\ Fonts/*.ttf
 
-cp "../otf/SystemFontText-Light.otf"           "Fira System Fonts/FSText-Light.otf"
-cp "../otf/SystemFontText-Regular.otf"         "Fira System Fonts/FSText-Regular.otf"
-cp "../otf/SystemFontText-RegularG1.otf"       "Fira System Fonts/FSText-RegularG1.otf"
-cp "../otf/SystemFontText-RegularG2.otf"       "Fira System Fonts/FSText-RegularG2.otf"
-cp "../otf/SystemFontText-RegularG3.otf"       "Fira System Fonts/FSText-RegularG3.otf"
-cp "../otf/SystemFontText-Medium.otf"          "Fira System Fonts/FSText-Medium.otf"
-cp "../otf/SystemFontText-Semibold.otf"        "Fira System Fonts/FSText-Semibold.otf"
-cp "../otf/SystemFontText-Bold.otf"            "Fira System Fonts/FSText-Bold.otf"
-cp "../otf/SystemFontText-BoldG1.otf"          "Fira System Fonts/FSText-BoldG1.otf"
-cp "../otf/SystemFontText-BoldG2.otf"          "Fira System Fonts/FSText-BoldG2.otf"
-cp "../otf/SystemFontText-BoldG3.otf"          "Fira System Fonts/FSText-BoldG3.otf"
-cp "../otf/SystemFontText-Heavy.otf"           "Fira System Fonts/FSText-Heavy.otf"
+cp "../ttf/FSDisplay.ttf"    "Fira System Fonts/FSDisplay.ttf"
+cp "../ttf/FSText.ttf"       "Fira System Fonts/FSText.ttf"
+cp "../ttf/FSTextItalic.ttf" "Fira System Fonts/FSTextItalic.ttf"
 
-cp "../otf/SystemFontText-LightItalic.otf"     "Fira System Fonts/FSText-LightItalic.otf"
-cp "../otf/SystemFontText-RegularItalic.otf"   "Fira System Fonts/FSText-RegularItalic.otf"
-cp "../otf/SystemFontText-RegularItalicG1.otf" "Fira System Fonts/FSText-RegularItalicG1.otf"
-cp "../otf/SystemFontText-RegularItalicG2.otf" "Fira System Fonts/FSText-RegularItalicG2.otf"
-cp "../otf/SystemFontText-RegularItalicG3.otf" "Fira System Fonts/FSText-RegularItalicG3.otf"
-cp "../otf/SystemFontText-MediumItalic.otf"    "Fira System Fonts/FSText-MediumItalic.otf"
-cp "../otf/SystemFontText-SemiboldItalic.otf"  "Fira System Fonts/FSText-SemiboldItalic.otf"
-cp "../otf/SystemFontText-BoldItalic.otf"      "Fira System Fonts/FSText-BoldItalic.otf"
-cp "../otf/SystemFontText-BoldItalicG1.otf"    "Fira System Fonts/FSText-BoldItalicG1.otf"
-cp "../otf/SystemFontText-BoldItalicG2.otf"    "Fira System Fonts/FSText-BoldItalicG2.otf"
-cp "../otf/SystemFontText-BoldItalicG3.otf"    "Fira System Fonts/FSText-BoldItalicG3.otf"
-cp "../otf/SystemFontText-HeavyItalic.otf"     "Fira System Fonts/FSText-HeavyItalic.otf"
-
-cp "../otf/SystemFontDisplay-Ultralight.otf"   "Fira System Fonts/FSDisplay-Ultralight.otf"
-cp "../otf/SystemFontDisplay-Thin.otf"         "Fira System Fonts/FSDisplay-Thin.otf"
-cp "../otf/SystemFontDisplay-Light.otf"        "Fira System Fonts/FSDisplay-Light.otf"
-cp "../otf/SystemFontDisplay-Regular.otf"      "Fira System Fonts/FSDisplay-Regular.otf"
-cp "../otf/SystemFontDisplay-Medium.otf"       "Fira System Fonts/FSDisplay-Medium.otf"
-cp "../otf/SystemFontDisplay-Semibold.otf"     "Fira System Fonts/FSDisplay-Semibold.otf"
-cp "../otf/SystemFontDisplay-Bold.otf"         "Fira System Fonts/FSDisplay-Bold.otf"
-cp "../otf/SystemFontDisplay-Heavy.otf"        "Fira System Fonts/FSDisplay-Heavy.otf"
-cp "../otf/SystemFontDisplay-Black.otf"        "Fira System Fonts/FSDisplay-Black.otf"
-
-cp "../otf/SystemFontSF-Regular.otf"           "Fira System Fonts/FSSF-Regular.otf"
-
-sudo chown root:wheel Fira\ System\ Fonts/*.otf
+sudo chown root:wheel Fira\ System\ Fonts/*.ttf
 
 # Disable non-installer download for now because people don't pay attention
 # to the correct file permissions
@@ -75,5 +41,5 @@ zip "fira-system-fonts-installer.zip" "Fira System Fonts.pkg"
 
 # clean up intermediate packages
 
-rm "Fira System Fonts.pkg"
-rm "fonts.pkg"
+#rm "Fira System Fonts.pkg"
+#rm "fonts.pkg"
